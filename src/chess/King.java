5bd82984c677eb73672f5564c29284c1f06a9f0c
Jacob <jcram@iastate.edu>
2018-02-22 20:16:36 -0600

    edited it

--- src/chess/King.java
+++ src/chess/King.java
@@ -9,12 +9,19 @@ public class King extends Piece {
 	@Override
 	public Tile[] getAvailableMoves(int row, int col, Tile[][] board) {
 		// TODO Auto-generated method stub
-		int[][] legalMoves = new int[8][8];
-		boolean flag = false;
-		
-		
-		
-		
-		return null;
+		Tile[] available = new Tile[32];
+		int i = 0;
+		for (int bRow = 0; bRow < 8; bRow++) {
+			for (int bCol = 0; bCol < 8; bCol++) {
+				if ((bRow == row - 1 && bCol == col) || (bRow == row + 1 && bCol == col)
+						|| (bRow == row && bCol == col - 1) || (bRow == row && bCol == col + 1)
+						|| (bRow == row - 1 && bCol == col - 1) || (bRow == row + 1 && bCol == col + 1)) {
+					available[i] = board[bRow][bCol];
+					i++;
+				}
+			}
+		}
+
+		return available;
 	}
 }
